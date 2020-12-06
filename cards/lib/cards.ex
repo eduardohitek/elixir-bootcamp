8756cf9369e2c9794a269df9997d29d5d6e8b013
Eduardo Hitek <eduardohitek@gmail.com>
2020-12-06 19:19:37 -0300

    Modulo 1
    Signed-off-by: Eduardo Hitek <eduardohitek@gmail.com>

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -18,6 +18,25 @@ defmodule Cards do
 
   @spec create_deck :: [<<_::24, _::_*16>>, ...]
   def create_deck do
-	  ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  @spec shuffle(any) :: [any]
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @spec contains?(any, any) :: boolean
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
