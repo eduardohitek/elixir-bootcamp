235027c74c716fb3ca195ec8074ed3634ddc7299
Eduardo Hitek <eduardohitek@gmail.com>
2021-01-21 08:20:49 -0300

    feat: finalizando o build_grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -19,10 +19,39 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    # [145, 46, 200, 46, 145]
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def pick_color(image) do
+    # %Identicon.Image{hex: [r, g, b | _tail]} = image
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list()
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
   end
 end
